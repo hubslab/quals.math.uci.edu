2b3255fd8488384027de2b1151b65d6dd9f518fd
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-04-21 23:40:48 +0000

    chore(*): added filler file

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="bg-blue-700 shadow-md opacity-90 text-white p-4 flex flex-row items-center gap-2">
-    <div class="bg-blue-500 p-4 rounded-full shadow-md">
+  <header class="bg-blue-700 shadow-md opacity-90 text-white p-3 flex flex-row items-center gap-2">
+    <div class="bg-blue-500 p-3 rounded-full shadow-md">
       <img class="w-10 h-10" src="/logo.webp" alt="Quals Logo">
     </div>
     <NuxtLink to="/">
