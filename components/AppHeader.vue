194f6be7005205d956711f5dff25951482179c03
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-04-21 18:22:31 +0000

    chore(icon): changed icon

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,8 @@
 <template>
-  <header class="bg-blue-700 shadow-md opacity-90 text-white p-4 flex flex-row">
+  <header class="bg-blue-700 shadow-md opacity-90 text-white p-4 flex flex-row items-center gap-2">
+    <div class="bg-blue-500 p-4 rounded-full shadow-md">
+      <img class="w-10 h-10" src="/logo.webp" alt="Quals Logo">
+    </div>
     <NuxtLink to="/">
       <div class="bg-blue-500 py-2 px-4 rounded-md text-xl font-bold hover:scale-105 transition shadow-md">
         Qualification
