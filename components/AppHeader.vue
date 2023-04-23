0129e08c7cdd20d09d7f0e7891ef4a66f32af51b
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-04-23 20:19:03 +0000

    chore(*): adjustments to text size

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,12 +1,13 @@
 <template>
-  <header class="bg-blue-700 shadow-md opacity-90 text-white px-4 py-3 flex flex-row items-center gap-2">
+  <header class="bg-blue-700 shadow-md opacity-90 text-white px-4 py-3 flex flex-row items-center gap-4">
     <div class="bg-blue-500 p-3 rounded-full shadow-md">
-      <img class="w-6 h-auto md:w-10" src="/logo.webp" alt="Quals Logo">
+      <img class="w-6 h-auto sm:w-10" src="/logo.webp" alt="Quals Logo">
     </div>
     <NuxtLink to="/">
       <div
         class="bg-blue-500 py-2 px-4
-      rounded-md text-xl font-bold hover:scale-105 transition shadow-md"
+      rounded-md text-md font-bold hover:scale-105 transition shadow-md
+      sm:text-xl"
       >
         UC Mathematics Qualifying Exams Home
       </div>
--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -5,7 +5,7 @@
     m-auto"
     >
       <ContentDoc
-        class="prose prose-lg max-w-none
+        class="prose prose-lg max-w-none prose-p:text-xl
         prose-headings:font-bold prose-headings:uppercase prose-a:font-bold
         prose-h1:text-center hover:prose-a:text-blue-400 prose-a:transition"
       />
