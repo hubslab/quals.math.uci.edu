29ed11bd3d3db26809283781ef3bbb392487a914
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-04-24 20:02:00 +0000

    feat(*): added loading bar to indicate loading progress

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex flex-col items-center font-cartoonist">
     <AppHeader class="sticky top-0 w-full z-50" />
+    <NuxtLoadingIndicator
+      color="repeating-linear-gradient(to right,#4ade80 0%,#a3e635 50%,#facc15 100%)"
+      :throttle="0"
+    />
     <main class="p-4 w-full">
       <slot />
     </main>
