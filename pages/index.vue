fa6ee72f008562664f342a9e44245d5113468df8
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-04-25 22:37:18 +0000

    fix(layout): unified layout entry

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,28 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 grid-cols-1">
-        <AppCard
-          v-for="entry in postMap"
-          :key="entry[0]"
-          class="bg-sky-100"
-        >
-          <h2 class="capitalize font-bold text-3xl">
-            {{ entry[0].replaceAll('-', ' ') }}
-          </h2>
-          <ul>
-            <li v-for="(item, index) in entry[1].sort(sortPosts)" :key="`${entry[0]}-${item.title}`">
-              <span class="w-6 inline-block">{{ index + 1 }}.</span>
-              <NuxtLink
-                :to="item.path"
-                class="capitalize hover:text-blue-400 transition text-xl"
-                :class="{
-                  'text-red-400': item.draft
-                }"
-              >
-                {{ item.title }}
-              </NuxtLink>
-            </li>
-          </ul>
-        </AppCard>
-      </div>
-    </NuxtLayout>
+  <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 grid-cols-1">
+    <AppCard
+      v-for="entry in postMap"
+      :key="entry[0]"
+      class="bg-sky-100"
+    >
+      <h2 class="capitalize font-bold text-3xl">
+        {{ entry[0].replaceAll('-', ' ') }}
+      </h2>
+      <ul>
+        <li v-for="(item, index) in entry[1].sort(sortPosts)" :key="`${entry[0]}-${item.title}`">
+          <span class="w-6 inline-block">{{ index + 1 }}.</span>
+          <NuxtLink
+            :to="item.path"
+            class="capitalize hover:text-blue-400 transition text-xl"
+            :class="{
+              'text-red-400': item.draft
+            }"
+          >
+            {{ item.title }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </AppCard>
   </div>
 </template>
 
--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -1,14 +1,10 @@
 <template>
-  <NuxtLayout>
-    <div
-      class="w-full lg:w-[1024px] m-auto"
-    >
-      <ContentDoc
-        class="prose prose-lg max-w-none prose-p:text-xl
+  <div class="w-full lg:w-[1024px] m-auto">
+    <ContentDoc
+      class="prose prose-lg max-w-none prose-p:text-xl
         prose-headings:font-bold prose-headings:uppercase prose-a:font-bold
         prose-h1:text-center hover:prose-a:text-blue-400 prose-a:transition
         prose-h1:my-2 flex flex-col items-center gap-4"
-      />
-    </div>
-  </NuxtLayout>
+    />
+  </div>
 </template>
