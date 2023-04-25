687a0e44f42b0cf4513c0b5f66f92c871cbe80eb
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-04-25 00:46:51 +0000

    build(*): swr mode building posts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content',
+  modules: [
+    '@nuxt/content',
     '@nuxtjs/tailwindcss'
   ],
-  vue: {
-    compilerOptions: {
-      // Workaround to suppress warnings on custom elements
-      // Ref: https://github.com/nuxt/content/issues/949
-      // Ref: https://github.com/nuxt/content/issues/1774
-      isCustomElement: (tag) => {
-        const tags = [
-          'math',
-          'annotation',
-          'semantics',
-          'mtext',
-          'mn',
-          'mo',
-          'mi',
-          'mspace',
-          'mover',
-          'munder',
-          'munderover',
-          'msup',
-          'msub',
-          'msubsup',
-          'mfrac',
-          'mroot',
-          'msqrt',
-          'mtable',
-          'mtr',
-          'mtd',
-          'mlabeledtr',
-          'mrow',
-          'menclose',
-          'mstyle',
-          'mpadded',
-          'mphantom',
-          'mglyph',
-          'svg',
-          'line',
-          'path']
-        return tags.includes(tag.toLowerCase())
-      }
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.webp' }],
+      titleTemplate: '%s | UC Math Qualifying Exams'
     }
   },
+  css: ['katex/dist/katex.min.css'],
   content: {
     markdown: {
       remarkPlugins: ['remark-math'],
@@ -52,12 +19,6 @@ export default defineNuxtConfig({
           '\\eps': '\\varepsilon'
         }
       }]]
-    }
-  },
-  app: {
-    head: {
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.webp' }],
-      titleTemplate: '%s | UC Math Qualifying Exams'
     }
   }
 })
