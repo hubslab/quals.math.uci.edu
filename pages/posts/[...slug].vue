b222e59e29da1dedd8b5fb104e2d9212dd5773b1
Zhiqin Lu <97719898+ZhiqinLuUCI@users.noreply.github.com>
2023-06-27 23:25:15 -0700

    feat(posts): 2015 f1 (#24)
    Co-authored-by: Zhiqin Lu <zhiqinlu@hemera.math.uci.edu>
    Co-authored-by: Kiyoshi <44930252+Kiyo5hi@users.noreply.github.com>

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -3,7 +3,8 @@
     <ContentDoc
       class="prose prose-lg max-w-none prose-p:text-xl prose-headings:uppercase
              prose-h1:text-problem prose-h1:text-center hover:prose-a:text-blue-400
-             prose-a:transition prose-h1:my-2 flex flex-col items-center gap-4"
+             prose-a:transition prose-h1:my-2 flex flex-col items-center gap-4
+             prose-p:my-1"
     />
   </div>
 </template>
